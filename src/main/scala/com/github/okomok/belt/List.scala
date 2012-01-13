31da38ca42c993e3169e429a0a4b1f7146d322b7
unknown <okomok@gmail.com>
2012-01-14 00:19:20 +0900

    Still doesn't work.

--- src/main/scala/com/github/okomok/belt/List.scala
+++ src/main/scala/com/github/okomok/belt/List.scala
@@ -8,21 +8,18 @@ package com.github.okomok
 package belt
 
 
-sealed abstract class List {
+sealed abstract class List { outer =>
     type head
      val head: head
     type tail <: List
      val tail: tail
 
-    type self = this.type
-     val self: self = this
-
     // Doesn't work for some reason.
     def ::(x: Any) = new List {
         override type head = x.type
         override  val head: head = x
-        override type tail = self
-        override  val tail: tail = self
+        override type tail = outer.type
+        override  val tail: tail = outer
     }
 }
 
@@ -31,9 +28,8 @@ object Nil extends List {
     override type head = Nothing
     override lazy val head: head = throw new NoSuchElementException("Nil.head")
     override type tail = Nothing
-    override lazy val tail: tail =  throw new UnsupportedOperationException("Nil.tail")
+    override lazy val tail: tail = throw new UnsupportedOperationException("Nil.tail")
 }
-
 
 object Cons {
     def apply(x: Any, xs: List) = new List {
